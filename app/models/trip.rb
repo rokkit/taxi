d213cef0476701789b27bb4835a598810bcb99f2
Maksim Ind <fss92@pochta.ru>
2013-07-29 12:50:05 +0400

    add sqllite

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,10 +2,11 @@ class Trip < ActiveRecord::Base
   belongs_to :client
   validates :client, presence: true
   after_create :windraw_bonus_points
-  accepts_nested_attributes_for :client
+  accepts_nested_attributes_for :client,reject_if: proc { |attributes| attributes['email'].blank? }
   def initialize(attributes = {})
     super
     self.trip_date = DateTime.now
+    self.client = Client.new
   end
 
   private
