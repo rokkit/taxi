ec075483fe79582d919e035a01b05f18f699a975
Maksim Ind <fss92@pochta.ru>
2013-07-31 15:05:56 +0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = Admin.create fio: "Администратор", email: 79217777777, password: 'password', password_confirmation: 'password', confirmed_at: DateTime.now
