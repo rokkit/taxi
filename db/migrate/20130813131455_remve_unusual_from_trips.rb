5559d09cc2b7f18716fcae06227186196683b5c6
Maxim Indykov <indmaksim@gmail.com>
2013-10-03 11:57:24 +0400

    change db to mssql

--- db/migrate/20130813131455_remve_unusual_from_trips.rb
+++ db/migrate/20130813131455_remve_unusual_from_trips.rb
@@ -1,8 +1,8 @@
 class RemveUnusualFromTrips < ActiveRecord::Migration
   def change
-    remove_column :trips, :trip_date
-    remove_column :trips, :price
-    remove_column :trips, :duration
-    remove_column :trips, :client_id
+    # remove_column :trips, :trip_date
+    # remove_column :trips, :price
+    # remove_column :trips, :duration
+    # remove_column :trips, :client_id
   end
 end
