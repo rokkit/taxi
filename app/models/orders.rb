27bb4bd09d9ae576744752f377b7afc53c0974c9
Maxim Indykov <indmaksim@gmail.com>
2013-10-11 18:10:35 +0400

    commit

--- app/models/orders.rb
+++ app/models/orders.rb
@@ -8,13 +8,13 @@ class Orders < ActiveRecord::Base
       total = 0
       total_spend_bonus_points = 0
       orders.each do |order|
+        total +=1
         unless order.trip.nil?
           total += order.cost_plan * (client.bonus_program.rate/100) if order.trip.bonus_point == 0
           total_spend_bonus_points += order.trip.bonus_point unless order.trip.bonus_point == 0
         end
       end
-      total - total_spend_bonus_points
-      4
+      total# - total_spend_bonus_points
    end
 
    def to_s
