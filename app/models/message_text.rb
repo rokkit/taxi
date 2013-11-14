cd1971d0b65c7d3dd84af4acb11bc4c7465e7368
Maxim Indykov <indmaksim@gmail.com>
2013-11-14 17:31:24 +0400

    fix

--- app/models/message_text.rb
+++ app/models/message_text.rb
@@ -1,6 +1,5 @@
 class MessageText < ActiveRecord::Base
   # scope :welcome, -> where(message_type: 1).first
-  establish_connection "messages"
   def self.welcome
     where(message_type: 1).first
   end
